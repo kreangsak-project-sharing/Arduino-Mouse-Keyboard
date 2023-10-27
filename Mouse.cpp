49c579c3710319c00ca943910cc5766beb17f891
Kreangsak <128896272+kreangsak-project-sharing@users.noreply.github.com>
2023-10-28 01:39:06 +0700

    Add files via upload

---
+++ Mouse.cpp
@@ -0,0 +1,185 @@
+/*
+  Mouse.cpp
+
+  Copyright (c) 2015, Arduino LLC
+  Original code (pre-library): Copyright (c) 2011, Peter Barrett
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "Mouse.h"
+
+#if defined(_USING_HID)
+
+static const uint8_t _hidReportDescriptor[] PROGMEM = {
+  
+  //  Mouse
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)  // 54
+    0x09, 0x02,                    // USAGE (Mouse)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x09, 0x01,                    //   USAGE (Pointer)
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x85, 0x01,                    //     REPORT_ID (1)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
+    0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x75, 0x05,                    //     REPORT_SIZE (5)
+    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x30,                    //     USAGE (X)
+    0x09, 0x31,                    //     USAGE (Y)
+    0x09, 0x38,                    //     USAGE (Wheel)
+    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
+    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
+    0x75, 0x08,                    //     REPORT_SIZE (8)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
+    0xc0,                          //   END_COLLECTION
+    0xc0,                          // END_COLLECTION
+	
+	#ifdef MOUSE_ABS
+	0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+	0x09, 0x02,        // Usage (Mouse)
+	0xA1, 0x01,        // Collection (Application)
+	0x09, 0x01,        //   Usage (Pointer)
+	0xA1, 0x00,        //   Collection (Physical)
+	0x85, 0x03,        //     Report ID (3)
+	0x05, 0x09,        //     Usage Page (Button)
+	0x19, 0x01,        //     Usage Minimum (0x01)
+	0x29, 0x03,        //     Usage Maximum (0x03)
+	0x15, 0x00,        //     Logical Minimum (0)
+	0x25, 0x01,        //     Logical Maximum (1)
+	0x95, 0x03,        //     Report Count (3)
+	0x75, 0x01,        //     Report Size (1)
+	0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0x95, 0x01,        //     Report Count (1)
+	0x75, 0x05,        //     Report Size (5)
+	0x81, 0x03,        //     Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0x05, 0x01,        //     Usage Page (Generic Desktop Ctrls)
+	0x09, 0x30,        //     Usage (X)
+	0x09, 0x31,        //     Usage (Y)
+	0x16, 0x00, 0x00,  //     Logical Minimum (0)
+	0x26, 0xFF, 0x7F,  //     Logical Maximum (32767)
+	0x36, 0x00, 0x00,  //     Physical Minimum (0)
+	0x46, 0xFF, 0x7F,  //     Physical Maximum (32767)
+	0x75, 0x10,        //     Report Size (16)
+	0x95, 0x02,        //     Report Count (2)
+	0x81, 0x02,        //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0xC0,              //   End Collection
+	0xC0               // End Collection
+	#endif
+};
+
+//================================================================================
+//================================================================================
+//	Mouse
+
+Mouse_::Mouse_(void) : _buttons(0), _x(0), _y(0)
+{
+    static HIDSubDescriptor node(_hidReportDescriptor, sizeof(_hidReportDescriptor));
+    HID().AppendDescriptor(&node);
+}
+
+void Mouse_::begin(void) 
+{
+}
+
+void Mouse_::end(void) 
+{
+}
+
+void Mouse_::click(uint8_t b)
+{
+	_buttons = b;
+	move(0,0,0);
+	_buttons = 0;
+	move(0,0,0);
+}
+
+void Mouse_::move(signed char x, signed char y, signed char wheel)
+{
+	uint8_t m[4];
+	m[0] = _buttons;
+	m[1] = x;
+	m[2] = y;
+	m[3] = wheel;
+	HID().SendReport(1,m,4);
+}
+
+//Mouse Absolute Start
+void Mouse_::init(uint16_t width, uint16_t height, bool autoReport)
+{
+	_width = width;
+	_height = height;
+	_autoReport = autoReport;
+}
+
+void Mouse_::report(void)
+{
+	uint8_t buffer[5];
+	buffer[0] = _buttons;
+	buffer[1] = _x & 0xFF;
+	buffer[2] = (_x >> 8) & 0xFF;
+	buffer[3] = _y & 0xFF;
+	buffer[4] = (_y >> 8) & 0xFF;
+	HID().SendReport(3, buffer, 5);
+}
+
+void Mouse_::moveABS(uint16_t x, uint16_t y)
+{
+	_x = (uint16_t) ((32767l * ((uint32_t) x)) / _width);
+	_y = (uint16_t) ((32767l * ((uint32_t) y)) / _height);
+
+	if (_autoReport) {
+		report();
+	}
+}
+//Mouse Absolute End
+
+void Mouse_::buttons(uint8_t b)
+{
+	if (b != _buttons)
+	{
+		_buttons = b;
+		move(0,0,0);
+	}
+}
+
+void Mouse_::press(uint8_t b) 
+{
+	buttons(_buttons | b);
+}
+
+void Mouse_::release(uint8_t b)
+{
+	buttons(_buttons & ~b);
+}
+
+bool Mouse_::isPressed(uint8_t b)
+{
+	if ((b & _buttons) > 0) 
+		return true;
+	return false;
+}
+
+Mouse_ Mouse;
+
+#endif
